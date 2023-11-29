5657245cc59f4a0a3fcb02fb01cdc302e9cb9382
S_a_b_b_i_R <sabbirssmc90@gmail.com>
2023-11-29 11:38:14 +0600

    featured tests (top 3 sold) done

--- src/Components/FeaturedTest/FeaturedTest.jsx
+++ src/Components/FeaturedTest/FeaturedTest.jsx
@@ -1,11 +1,22 @@
-import useSlot from "../../hooks/useSlot";
+import { useQuery } from "@tanstack/react-query";
 import Container from "../Container/Container";
 import Loading from "../Loading/Loading";
 import FeaturedTestCard from "./FeaturedTestCard";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
 
 const FeaturedTest = () => {
-    const { slotData, slotLoading } = useSlot();
+    const axiosSecure = useAxiosSecure();
+    const { data: slotData, isPending: slotLoading } = useQuery({
+        queryKey: ["slots"],
+        queryFn: async () => {
+            const res = await axiosSecure.get("/topbookings");
+            return res.data.topThreeTests;
+        },
+    });
+
     if (slotLoading) return <Loading />;
+
+    console.log(slotData);
     return (
         <Container>
             <div className="space-y-6 my-6">
@@ -13,8 +24,8 @@ const FeaturedTest = () => {
                     Featured Tests
                 </h1>
                 <div className="divider w-1/3 mx-auto"></div>
-                <div className="grid grid-cols-2 gap-2 mx-5">
-                    {slotData.map((test) => (
+                <div className="grid grid-cols-3 gap-2 mx-5">
+                    {slotData?.map((test) => (
                         <FeaturedTestCard key={test._id} test={test} />
                     ))}
                 </div>
--- src/Components/FeaturedTest/FeaturedTestCard.jsx
+++ src/Components/FeaturedTest/FeaturedTestCard.jsx
@@ -3,11 +3,11 @@ const FeaturedTestCard = ({ test }) => {
     return (
         <div className="card bg-base-200 ">
             <div className="card-body">
-                <h2 className="card-title">{test.title}</h2>
-                <p>{test.details}</p>
+                <h2 className="card-title">{test.test.title}</h2>
+                <p>{test.test.details}</p>
                 <p>
                     <span>Prep: </span>
-                    {test.preparation}
+                    {test.test.preparation}
                 </p>
             </div>
         </div>
